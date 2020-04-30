f952a8b8628d3799dc1e0f9a843838990271a188
cozyBoi <kjjh1126@sogang.ac.kr>
2020-04-30 12:45:44 +0900

    add debug

--- HW1.c
+++ HW1.c
@@ -265,6 +265,7 @@ int main() {
 		}
         //printf("before read\n");
 		read(dev, &push_sw_buff, buff_size);
+        usleep(400000);
         //printf("after read\n");
 		if (mode == 0) {
 			//boradÀÇ ½Ã°£À» °¡Á®¿Í¾ßÇÔ
@@ -278,23 +279,23 @@ int main() {
             }
 			if (push_sw_buff[0] == 1) {
 				Text_mode = 0;
-                push_sw_buff[0] = 0;
+//                push_sw_buff[0] = 0;
 			}
 			else if (push_sw_buff[1] == 1) {
                 Clock_FND_set_to_borad_time();
-                push_sw_buff[1] = 0;
+//                push_sw_buff[1] = 0;
 			}
 			else if (push_sw_buff[2] == 1) {
                 clock_plus_hour();
                 printf("plus hour\n");
                 Text_mode = 1;
-                push_sw_buff[2] = 0;
+//                push_sw_buff[2] = 0;
 			}
 			else if (push_sw_buff[3] == 1) {
 				clock_plus_minute();
                 printf("plus minute\n");
                 Text_mode = 1;
-                push_sw_buff[3] = 0;
+//                push_sw_buff[3] = 0;
 			}
             out_to_FND(FND);
 		}
